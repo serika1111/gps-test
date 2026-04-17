8a2ecb5d29bbc92726d1939ba0cf4c2387beeab2
serika1111 <87085285+serika1111@users.noreply.github.com>
2026-04-17 10:52:05 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,37 @@
+const CACHE_NAME = "driver-v1";
+
+// install
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {});
+// activate (წაშლის ძველ cache-ებს)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch (online first, fallback cache)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
